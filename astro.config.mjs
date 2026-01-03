83a0b5011dd18f1cebcf8af6beef7b8a5e349657
alonsolmz <alonsoalmerco425@gmail.com>
2026-01-03 15:31:23 -0500

    Despliegue final MaquiPeru

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        'azul-principal': '#0B132B',
-        'amarillo-acento': '#F2B705',
-      }
-    },
-  },
-  plugins: [],
-}
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless'; 
+
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server', 
+  adapter: vercel(), 
+});
